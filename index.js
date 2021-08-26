15288c3360349048b35889d3a010f261a22f29ef
RosembergAraujo <rosembergv.filho@gmail.com>
2021-08-26 02:07:22 -0300

    VERCEL

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv/config')
 const express = require('express');
 const app = express();
  
+
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -11,4 +14,4 @@ app.get('/', (req, res) => {
 require('./app/controllers/index')(app)
 
 
-module.exports= app
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
