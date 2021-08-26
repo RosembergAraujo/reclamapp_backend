969dfe30f5369c7dc1b061794a2c4c0d6415e33d
RosembergAraujo <rosembergv.filho@gmail.com>
2021-08-26 02:09:12 -0300

    VERCEL

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('FDS')
 })
 
-require('./app/controllers/index')(app)
+require('./src/app/controllers/index')(app)
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
