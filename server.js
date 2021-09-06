7aa27afac57d46373b52380a539b55b4d2e8aa20
Rosemberg Araujo <70284263+RosembergAraujo@users.noreply.github.com>
2021-09-06 07:22:04 -0300

    .

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(morgan('dev'))
 /* ROUTES */
 require('./src/app/controllers/index')(app)
 
+
+app.get('/', async(req,res)=> {
+    res.sendFile('./splash.png')
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Listen on ${process.env.PORT}`)
 })
